1d4aaeddbd40932e856f2d44ebb82d425b47eb59
Slava Kim <slv@mit.edu>
2016-08-23 08:58:19 -0400

    more house stuff

--- pages/events.js
+++ pages/events.js
@@ -14,7 +14,20 @@ import CoverImg from '../static/events-cover.jpg';
 export default function (props) {
   return (
       <DocumentTitle title={"Events | " + config.siteTitle}>
-        <Splash id="events" imageUrl={CoverImg}></Splash>
+        <div>
+          <Splash id="events" imageUrl={CoverImg}></Splash>
+            <div className="contents typography">
+      <div className="important full-size">If you are a freshman or a sophomore looking to rush ZBT, make sure to look at the <Link to={prefixLink('/rush/')}>rush page</Link> for fall events and activites.</div>
+              <h2>Parties</h2>
+              <p>Everyone needs a break from work, and being MIT students we know well how difficult the work can be. ZBT house is an often host of social events and parties, an opportunity for brothers to hang out with friends, relax and to have a good time.</p>
+              <p>ZBT throws a dozen of big parties open to anyone in MIT community and friends over the duration of the school year. It is a great place to get to know the ZBT community, make new friends or show off your latest dance moves.</p>
+              <h2>Formal Events</h2>
+              <p>Every semester brothers have an opportunity to bring a date to a semi-annual formal event. Events range from a nice special dinner to a boat cruise on the Charles River.</p>
+              <p>Another formal event is Soiree. It is an invite-only evening at the house with fancy desserts, drinks and a karaoke night to follow.</p>
+              <h2>Dinners</h2>
+              <p>We invite our friends to dinners at the house on workdays</p>
+            </div>
+        </div>
       </DocumentTitle>
   );
 }
--- pages/house.js
+++ pages/house.js
@@ -13,6 +13,7 @@ import {
 import CoverImg from '../static/house-cover.jpg';
 import CommonsImg from '../static/commons.jpg';
 import HouseImg from '../static/house.jpg';
+import VanImg from '../static/van.jpg';
 
 import { prefixLink } from '../utils/urls.js'
 
@@ -31,15 +32,17 @@ export default function (props) {
               </div>
             </Split>
             <Row id="commons" flipped imageUrl={CommonsImg}>The house includes two floors with residencies. 18 rooms total. Common areas include: Commons, Chapter Room, Dinings, Kitchen, Party Room, Game Room, TV Room and Athena Cluster.</Row>
+      </div>
 
+          <Row imageUrl={VanImg} id="house-img">
             <h2>Transportation</h2>
             <p>During the school time brothers drive the house van to and from MIT campus multiple times a day on the schedule.</p>
-      <p>At the late hours, starting at 6pm and up until 2:30am you can take a <a href="http://web.mit.edu/facilities/transportation/shuttles/safe_ride.html">Saferide bus</a> provided by MIT. Take the Cambridge West & Brookline bus which stops in front of the house.</p>
+            <p>At the late hours, starting at 6pm and up until 2:30am you can take a <a href="http://web.mit.edu/facilities/transportation/shuttles/safe_ride.html">Saferide bus</a> provided by MIT. Take the Cambridge West & Brookline bus which stops in front of the house.</p>
+            </Row>
 
-            <h2>Summer Housing</h2>
-          </div>
 
-            <Row imageUrl={HouseImg} id="house-img">
+            <Row flipped imageUrl={HouseImg} id="house-img">
+              <h2>Summer Housing</h2>
               <p>Email zbt-president@mit.edu to apply.</p>
               <p>Summer housing includes room and access to all common areas listed here, including a fully equipped industrial kitchen, laundry room, weight room, 40'' TV for watching movies or shows, newly furnished study space, and Athena cluster.</p>
               <p>We're minutes away from local parks and local commercial plazas including Coolidge Corner. We are also a 3-minute walk from a 24-hour supermarket and a 5-minute walk from the MBTA Green Line. There will be daily van runs to and from MIT's campus, as well as the MIT Saferide shuttle every hour after 6pm.</p>
