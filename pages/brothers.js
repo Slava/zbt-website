378152d7a776ee6274860ab89931d9d1c53893f6
Allen Ding <aqding@mit.edu>
2022-08-31 20:51:12 -0400

    adding rhos and updating rush

--- pages/brothers.js
+++ pages/brothers.js
@@ -1,57 +1,77 @@
-import React from 'react'
-import DocumentTitle from 'react-document-title'
-import SkyLight from 'react-skylight'
-import Tabs from 'react-simpletabs'
-import '../node_modules/react-simpletabs/lib/react-simpletabs.css'
-import '../css/tabs.css'
+import React from "react";
+import DocumentTitle from "react-document-title";
+import SkyLight from "react-skylight";
+import Tabs from "react-simpletabs";
+import "../node_modules/react-simpletabs/lib/react-simpletabs.css";
+import "../css/tabs.css";
 
-import { config } from 'config'
+import { config } from "config";
 
-import { prefixLink } from '../utils/urls.js'
+import { prefixLink } from "../utils/urls.js";
 
-import {
-  Row,
-  Splash,
-  Split,
-  ImgGallery,
-} from './_sharedComponents'
+import { Row, Splash, Split, ImgGallery } from "./_sharedComponents";
 
-const brothersInfo = require('./_brothers-data.js')
+const brothersInfo = require("./_brothers-data.js");
 
-import CoverImg from '../static/brothers-cover.jpg';
-import BrothersImg1 from '../static/brothers-1.jpg';
-import BrothersImg2 from '../static/brothers-2.jpg';
-import BrothersImg3 from '../static/brothers-3.jpg';
-import BrothersImg4 from '../static/brothers-4.jpg';
-import BrothersImg5 from '../static/brothers-5.jpg';
-import BrothersImg6 from '../static/brothers-6.jpg';
-import BrothersImg7 from '../static/brothers-7.jpg';
-import BrothersImg8 from '../static/brothers-8.jpg';
-import BrothersImg9 from '../static/brothers-9.jpg';
-import BrothersImg10 from '../static/brothers-10.jpg';
-import BrothersImg11 from '../static/brothers-11.jpg';
-import BrothersImg12 from '../static/brothers-12.jpg';
+import CoverImg from "../static/brothers-cover.jpg";
+import BrothersImg1 from "../static/brothers-1.jpg";
+import BrothersImg2 from "../static/brothers-2.jpg";
+import BrothersImg3 from "../static/brothers-3.jpg";
+import BrothersImg4 from "../static/brothers-4.jpg";
+import BrothersImg5 from "../static/brothers-5.jpg";
+import BrothersImg6 from "../static/brothers-6.jpg";
+import BrothersImg7 from "../static/brothers-7.jpg";
+import BrothersImg8 from "../static/brothers-8.jpg";
+import BrothersImg9 from "../static/brothers-9.jpg";
+import BrothersImg10 from "../static/brothers-10.jpg";
+import BrothersImg11 from "../static/brothers-11.jpg";
+import BrothersImg12 from "../static/brothers-12.jpg";
 
-const brothersPhotos = [BrothersImg1, BrothersImg2, BrothersImg3, BrothersImg4, BrothersImg5, BrothersImg6, BrothersImg7, BrothersImg8, BrothersImg9, BrothersImg10, BrothersImg11, BrothersImg12 ].map(url => ({src: prefixLink(url)}));
+const brothersPhotos = [
+  BrothersImg1,
+  BrothersImg2,
+  BrothersImg3,
+  BrothersImg4,
+  BrothersImg5,
+  BrothersImg6,
+  BrothersImg7,
+  BrothersImg8,
+  BrothersImg9,
+  BrothersImg10,
+  BrothersImg11,
+  BrothersImg12,
+].map((url) => ({ src: prefixLink(url) }));
 
 export default function (props) {
   let _brother;
   let openBrotherFn = () => {};
 
-  const onSelect = (brotherDoc) => _brother.setState(brotherDoc) + openBrotherFn();
+  const onSelect = (brotherDoc) =>
+    _brother.setState(brotherDoc) + openBrotherFn();
 
   return (
-      <DocumentTitle title={"Brothers | " + config.siteTitle}>
-        <div>
-          <Splash id="brothers" imageUrl={CoverImg}/>
-          <div className="contents typography">
-            <h1>Brothers</h1>
-            <p>ZBT is a non-pledging, historically Jewish fraternity open to all men of good character. Our house attracts smart and energetic young men interested in sciences and engineering with wide range of athletic and academic interests. Many brothers are very active on campus, leading various student organisations and clubs, dancing in troups, trading, playing poker and teaching web development to the wider MIT community. Many brothers are also involved in student clubs working with kids in Boston area such as <a href="http://campkesem.org/mit">Camp Kesem</a> and <a href="http://mitdynamit.weebly.com/">DynaMIT</a>.</p>
-            <ImgGallery images={brothersPhotos} useLightbox />
-          </div>
-          <hr/>
+    <DocumentTitle title={"Brothers | " + config.siteTitle}>
+      <div>
+        <Splash id="brothers" imageUrl={CoverImg} />
+        <div className="contents typography">
+          <h1>Brothers</h1>
+          <p>
+            ZBT is a non-pledging, historically Jewish fraternity open to all
+            men of good character. Our house attracts smart and energetic young
+            men interested in sciences and engineering with wide range of
+            athletic and academic interests. Many brothers are very active on
+            campus, leading various student organisations and clubs, dancing in
+            troups, trading, playing poker and teaching web development to the
+            wider MIT community. Many brothers are also involved in student
+            clubs working with kids in Boston area such as{" "}
+            <a href="http://campkesem.org/mit">Camp Kesem</a> and{" "}
+            <a href="http://mitdynamit.weebly.com/">DynaMIT</a>.
+          </p>
+          <ImgGallery images={brothersPhotos} useLightbox />
+        </div>
+        <hr />
         <Tabs tabActive={4}>
-            {/* <Tabs.Panel title="βθ">
+          {/* <Tabs.Panel title="βθ">
               <ClassGallery classid="BetaTheta" onSelect={onSelect}/>
             </Tabs.Panel>
             <Tabs.Panel title="βι">
@@ -66,41 +86,51 @@ export default function (props) {
             <Tabs.Panel title="βμ">
               <ClassGallery classid="BetaMu" onSelect={onSelect}/>
             </Tabs.Panel> */}
-            <Tabs.Panel title="βν">
-              <ClassGallery classid="BetaNu" onSelect={onSelect}/>
-            </Tabs.Panel>
-            <Tabs.Panel title="βξ">
-              <ClassGallery classid="BetaXi" onSelect={onSelect}/>
-            </Tabs.Panel>
-            <Tabs.Panel title="βο">
-              <ClassGallery classid="BetaOmicron" onSelect={onSelect}/>
-            </Tabs.Panel>
-            <Tabs.Panel title="βπ">
-              <ClassGallery classid="BetaPi" onSelect={onSelect}/>
-            </Tabs.Panel>
+          {/* <Tabs.Panel title="βν">
+            <ClassGallery classid="BetaNu" onSelect={onSelect} />
+          </Tabs.Panel> */}
+          <Tabs.Panel title="βξ">
+            <ClassGallery classid="BetaXi" onSelect={onSelect} />
+          </Tabs.Panel>
+          <Tabs.Panel title="βο">
+            <ClassGallery classid="BetaOmicron" onSelect={onSelect} />
+          </Tabs.Panel>
+          <Tabs.Panel title="βπ">
+            <ClassGallery classid="BetaPi" onSelect={onSelect} />
+          </Tabs.Panel>
+          <Tabs.Panel title="βρ">
+            <ClassGallery classid="BetaRho" onSelect={onSelect} />
+          </Tabs.Panel>
         </Tabs>
-          <BrotherPane ref={(ref) => _brother = ref} cb={(f) => openBrotherFn = f}/>
-        </div>
-      </DocumentTitle>
+        <BrotherPane
+          ref={(ref) => (_brother = ref)}
+          cb={(f) => (openBrotherFn = f)}
+        />
+      </div>
+    </DocumentTitle>
   );
 }
 
 class BrotherPane extends React.Component {
   constructor() {
     super();
-    this.state = { name: '', classname: '', bio: '' };
+    this.state = { name: "", classname: "", bio: "" };
   }
 
   render() {
     this.props.cb(() => this.refs.brotherPopup.show());
     return (
-        <SkyLight hideOnOverlayClicked ref="brotherPopup" title={this.state.title}>
-          <div className="contents typography">
-            <h2>{this.state.name}</h2>
-            <h3>{this.state.classname}</h3>
-            <p className="brother-bio">{this.state.bio}</p>
-          </div>
-        </SkyLight>
+      <SkyLight
+        hideOnOverlayClicked
+        ref="brotherPopup"
+        title={this.state.title}
+      >
+        <div className="contents typography">
+          <h2>{this.state.name}</h2>
+          <h3>{this.state.classname}</h3>
+          <p className="brother-bio">{this.state.bio}</p>
+        </div>
+      </SkyLight>
     );
   }
 }
@@ -109,28 +139,36 @@ class ClassGallery extends React.Component {
   render() {
     const { classid } = this.props;
 
-    const brothers = brothersInfo.filter(b => b.classid === classid);
+    const brothers = brothersInfo.filter((b) => b.classid === classid);
     const brothersCards = brothers.map((brotherDoc) => {
-      const {imgurl, name, bio} = brotherDoc;
+      const { imgurl, name, bio } = brotherDoc;
       return (
-
         <div key={name} className="brother-card">
-            <a onClick={(e) => {e.preventDefault(); this.props.onSelect(brotherDoc)}}>
-
-            <img src={imgurl}/>
+          <a
+            onClick={(e) => {
+              e.preventDefault();
+              this.props.onSelect(brotherDoc);
+            }}
+          >
+            <img
+              src={imgurl}
+              style={{
+                width: "200px",
+                height: "300px",
+                objectFit: "cover",
+              }}
+            />
             <span className="name">{name}</span>
-            </a>
+          </a>
         </div>
       );
     });
 
     return (
-        <div className="class-gallery">
-          <h2>{brothers[0].classname}</h2>
-          <div className="cards">
-            {brothersCards}
-          </div>
-        </div>
+      <div className="class-gallery">
+        <h2>{brothers[0].classname}</h2>
+        <div className="cards">{brothersCards}</div>
+      </div>
     );
   }
 }
--- pages/rush.js
+++ pages/rush.js
@@ -1,56 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router'
-import DocumentTitle from 'react-document-title'
-import SkyLight from 'react-skylight'
-import { config } from 'config'
+import React from "react";
+import { Link } from "react-router";
+import DocumentTitle from "react-document-title";
+import SkyLight from "react-skylight";
+import { config } from "config";
 
-import {
-  Row,
-  Splash,
-  ImgGallery,
-} from './_sharedComponents'
+import { Row, Splash, ImgGallery } from "./_sharedComponents";
 
-import { prefixLink } from '../utils/urls.js'
-import CoverImg from '../static/rush-cover.jpg';
+import { prefixLink } from "../utils/urls.js";
+import CoverImg from "../static/rush-cover.jpg";
 
-import RushImg1 from '../static/rush-photo-1.jpg';
-import RushImg2 from '../static/rush-photo-2.jpg';
-import RushImg3 from '../static/rush-photo-3.jpg';
-import RushImg4 from '../static/rush-photo-4.jpg';
-import RushImg5 from '../static/rush-photo-5.jpg';
-import RushImg6 from '../static/rush-photo-6.jpg';
+import RushImg1 from "../static/rush-photo-1.jpg";
+import RushImg2 from "../static/rush-photo-2.jpg";
+import RushImg3 from "../static/rush-photo-3.jpg";
+import RushImg4 from "../static/rush-photo-4.jpg";
+import RushImg5 from "../static/rush-photo-5.jpg";
+import RushImg6 from "../static/rush-photo-6.jpg";
 
-const rushPhotos = [RushImg1, RushImg2, RushImg3, RushImg4, RushImg5, RushImg6].map(url => ({src: prefixLink(url)}));
+const rushPhotos = [
+  RushImg1,
+  RushImg2,
+  RushImg3,
+  RushImg4,
+  RushImg5,
+  RushImg6,
+].map((url) => ({ src: prefixLink(url) }));
 
 export default function ({}) {
   return (
-      <DocumentTitle title={"Rush ZBT | " + config.siteTitle}>
-        <div>
-          <Splash id="rush" imageUrl={CoverImg}></Splash>
-          <div className="contents typography">
-            <h1>Rush ZBT this fall! (2021)</h1>
-            <WithBoringTextOnSide>
-              <Schedule/>
-            </WithBoringTextOnSide>
-            <ImgGallery images={rushPhotos} useLightbox />
-          </div>
-          <div className="contents typography">
-            <h2></h2>
-          </div>
+    <DocumentTitle title={"Rush ZBT | " + config.siteTitle}>
+      <div>
+        <Splash id="rush" imageUrl={CoverImg}></Splash>
+        <div className="contents typography">
+          <h1>Rush ZBT this fall! (2022)</h1>
+          <WithBoringTextOnSide>
+            <Schedule />
+          </WithBoringTextOnSide>
+          <ImgGallery images={rushPhotos} useLightbox />
         </div>
-      </DocumentTitle>
+        <div className="contents typography">
+          <h2></h2>
+        </div>
+      </div>
+    </DocumentTitle>
   );
 }
 
 // this was in the flex-cell div: <p className="important">Call ZBT for rides at <a href="tel:6172323257">617-404-9663</a> to get to any Rush event.</p>
-function WithBoringTextOnSide ({children}) {
+function WithBoringTextOnSide({ children }) {
   return (
-      <div className="flex rush">
-        <div className="flex-cell">
-        * invite only
-        </div>
-        <div className="flex-cell-bigger schedule">{children}</div>
-      </div>
+    <div className="flex rush">
+      <div className="flex-cell">* invite only</div>
+      <div className="flex-cell-bigger schedule">{children}</div>
+    </div>
   );
 }
 
@@ -60,18 +61,23 @@ function Schedule() {
 
   const events = parseText();
   const byDay = {};
-  events.forEach(x => { byDay[x.day] = byDay[x.day] ? [...byDay[x.day], x] : [x] });
+  events.forEach((x) => {
+    byDay[x.day] = byDay[x.day] ? [...byDay[x.day], x] : [x];
+  });
 
   const days = [];
   Object.keys(byDay).forEach((key) => {
-    days.push({ date: new Date(key.split(', ')[1] + ' 2018'), key });
+    days.push({ date: new Date(key.split(", ")[1] + " 2018"), key });
   });
 
   days.sort((a, b) => -a.date + b.date);
   const tableRows = [];
-  days.forEach(({key}) => {
+  days.forEach(({ key }) => {
     tableRows.push(
-      <tr key={key}><th></th><th>{key}</th></tr>
+      <tr key={key}>
+        <th></th>
+        <th>{key}</th>
+      </tr>
     );
     byDay[key].forEach((x, i) => {
       const deets = (e) => {
@@ -81,168 +87,146 @@ function Schedule() {
       };
       tableRows.push(
         <tr key={key + i}>
-          <td className="time">{x['time']}</td>
-          <td>{x['title']} <a href="#" onClick={deets}>details</a></td>
+          <td className="time">{x["time"]}</td>
+          <td>
+            {x["title"]}{" "}
+            <a href="#" onClick={deets}>
+              details
+            </a>
+          </td>
         </tr>
       );
     });
   });
 
   return (
-      <div className="schedule">
-        <DetailsPane ref={(ref) => _details = ref } cb={(f) => openDeetsFn = f}/>
-        <table>
-        <tbody>
-          {tableRows}
-          </tbody>
-        </table>
-        {/*<div className="note">Events marked with asterisk(*) are invite-only</div>*/}
-      </div>
+    <div className="schedule">
+      <DetailsPane
+        ref={(ref) => (_details = ref)}
+        cb={(f) => (openDeetsFn = f)}
+      />
+      <table>
+        <tbody>{tableRows}</tbody>
+      </table>
+      {/*<div className="note">Events marked with asterisk(*) are invite-only</div>*/}
+    </div>
   );
 }
 
 class DetailsPane extends React.Component {
   constructor() {
     super();
-    this.state = { title: '', day: '', time: '', desc: '' };
+    this.state = { title: "", day: "", time: "", desc: "" };
   }
 
   render() {
     this.props.cb(() => this.refs.deetsPopup.show());
     return (
-          <SkyLight hideOnOverlayClicked ref="deetsPopup" title={this.state.title}>
-            <h3>Time: {this.state.time} - {this.state.day}</h3>
-            <h3>Location: {this.state.location}</h3>
-            <p>{this.state.desc}</p>
-          </SkyLight>
+      <SkyLight hideOnOverlayClicked ref="deetsPopup" title={this.state.title}>
+        <h3>
+          Time: {this.state.time} - {this.state.day}
+        </h3>
+        <h3>Location: {this.state.location}</h3>
+        <p>{this.state.desc}</p>
+      </SkyLight>
     );
   }
 }
 
 function parseText() {
-  return text.split('\n\n').map((paragraph) => {
-    const lines = paragraph.split('\n');
+  return text.split("\n\n").map((paragraph) => {
+    const lines = paragraph.split("\n");
     return {
-      title: lines[0].split(': ').slice(1).join(': '),
-      time: lines[1].split(', ').slice(2).join(', '),
-      day: lines[1].split(', ').slice(0, 2).join(', ').split(': ').slice(1).join(': '),
-      location: lines[2].split(': ').slice(1).join(': '),
-      desc: lines[4]
+      title: lines[0].split(": ").slice(1).join(": "),
+      time: lines[1].split(", ").slice(2).join(", "),
+      day: lines[1]
+        .split(", ")
+        .slice(0, 2)
+        .join(", ")
+        .split(": ")
+        .slice(1)
+        .join(": "),
+      location: lines[2].split(": ").slice(1).join(": "),
+      desc: lines[4],
     };
   });
 }
 
-const text = `Title: BBQ with ZBT
-Time: Saturday, September 4, 1:00pm - 4:00pm
-Location: Walker
+const text = `Title: Kresge Kickoff
+Time: Saturday, September 3, 12:00pm - 2:00pm
+Location: Kresge Lawn
 Description:
-Come chill with ZBT and eat some BBQ chicken and snacks! Vegetarian options available. Enjoy the great food, music, and company, or join us for some games of spikeball!
+Come chill with ZBT and get to know the brothers as we kick off the rush season!
 
-Title: Dinner in the City
-Time: Saturday, September 4, 6:00pm - 8:00pm
-Location: Walker
+Title: Raising Canes and Outdoor Games
+Time: Saturday, September 3, 2:00pm - 5:00pm
+Location: Briggs Field
 Description:
-Dinner with ZBT! Come hang out with brothers and friends. Vegetarian options will be available as well!
+The infamous Canes chicken and outdoor games with ZBT! Come hang out with brothers and friends. Vegetarian options will be available as well!
 
-Title: Night in the City
-Time: Saturday, September 4, 8:00pm - 12:00am
-Location: Walker
+Title: ZBT Sushi Night
+Time: Saturday, September 3, 6:00pm - 8:00pm
+Location: BC Porter Room
 Description:
-Try your luck with the brothers of ZBT playing some of the most popular games enjoyed on a Saturday night, right here in Walker Memorial! Hundreds of dollars in prizes are available for you to win!
+Think you can beat the brothers at a sushi eating contest? Stop by for some delicious sushi (vegetarian options available) and a great time with the brothers!
 
-Title: ZBT Breakfast of Champions
-Time: Sunday, September 5, 9:00am - 12:00pm
-Location: Student Center Steps
+Title: ZBT Poker Night
+Time: Saturday, September 3, 8:00pm - 12:00am
+Location: BC Porter Room
 Description:
-Come to the stud for ZBT’s Breakfast of Champions! Eggs, bacon, sausage, fresh fruit and muffins for days. Vegetarians welcome! Call us at 617-420-2928
-
-Title: Battle Canoeing
-Time: Sunday, September 5, 12:00pm - 4:30pm
-Location: Meet at Student Center Steps
-Description:
-This is SPARTA!! Actually, it’s just the Charles river. Wanna get soaked in the river just for the fun of it? Wanna throw some water balloons at people because why not? Come to ZBT Battle Canoeing! We will be renting boats to go on the Charles. Life jackets will be available and your electronics will be taken care of. Call us at 617-420-2928 for rides.
-
-Title: Dinner with ZBT: Taste of Italy
-Time: Sunday, September 5, 5:00pm - 6:30pm
-Location: ZBT
-Description:
-His palms are sweaty, knees weak, arms are heavy...mom's spaghetti; He's nervous, but on the surface he looks calm and ready; To go to ZBT for Taste of Italy! We will have our own ITALIAN (first order approximation) chef cook spaghetti and meatballs, chicken parmesan, desserts, and other snacks. Vegetarians welcome! We will also have games such as pinball, foosball, etc! Call us at 617-420-2928.
-
-Title: Laser Tag
-Time: Sunday, September 5, 6:30pm - 9:00pm
-Location: Meet at Walker
-Description:
-Come battle for victory with laser tag! We will meet at Walker and go to Xtreme Craze for a fun, competitive time. Call us at 617-420-2928 for rides.
-
-Title: ZBT Breakfast of Champions
-Time: Monday, September 6, 9:00am - 12:00pm
-Location: Student Center Steps
-Description:
-Come to the stud for ZBT’s Breakfast of Champions! Eggs, bacon, sausage, fresh fruit and muffins for days. Vegetarians welcome! Call us at 617-420-2928
+Try your luck with the brothers of ZBT playing some classic, no limit, texas hold'em. Come for a chance to win some amazing prizes, such as TVs and AirPods! Maybe you'll even face off against some members of the MIT Poker Club...
 
 Title: Paintball
-Time: Monday, September 6, 12:00pm - 4:30pm
-Location: Meet at Student Center Steps
+Time: Sunday, September 4, 11:00am - 4:00pm
+Location: Meet at Kresge Turnaround
 Description:
-Splatter your opponents with paint on the battlefield. Join our ranks to show your strength in glorious battle. Provisions will be provided, thanks to our chef, with vegetarian options available. Extra t-shirts will be available, but it is recommended to bring clothes you don’t mind getting dirty. Meet at the Student Center to start your journey. Call us at 617-420-2928 for your ride!
+Splatter your opponents with paint on the battlefield. Join our ranks to show your strength in glorious battle. Provisions will be provided to all soldiers, thanks to our chef, with vegetarian options available. Make sure to bring clothes you don't mind getting dirty!
 
-Title: Dinner with ZBT: Steak Night
-Time: Monday, September 6, 6:00pm - 8:00pm
+Title: House Tours and Thai Food
+Time: Sunday, September 4, 5:30pm - 7:30pm
+Location: ZBT
+Description: 
+Come join the brothers at the house for Thai food and house tours! Chill with the brothers and friends, and get to see the ZBT house for yourself. Vegetarian options available, and Muay Thai is discouraged but not illegal.
+
+Title: Minigolf
+Time: Sunday, September 4, 7:30pm - 10:00pm
+Location: Meet at ZBT (from dinner) or Kresge Turnaround
+Description: 
+Come and join the brothers for a fun night of minigolf! A night filled with missing swings, eating dirt, and digging golf balls out of bushes awaits you.
+
+Title: Battle Canoeing
+Time: Monday, September 5, 12:00pm - 4:00pm
+Location: Meet at Kresge Turnaround
+Description:
+This is SPARTA!! Actually, it’s just the Charles river. Wanna get soaked in the river just for the fun of it? Wanna throw some water balloons at people because why not? Come to ZBT Battle Canoeing! We will be renting boats to go on the Charles. Life jackets will be available and your electronics will be taken care of.
+
+Title: Dinner at the House: Korean Fried Chicken
+Time: Monday, September 5, 5:30pm - 7:00pm
 Location: ZBT
 Description:
-Steak dinner with ZBT at the house. Come hang out with brothers and friends. Vegetarian options will be available as well!
+Wanna know how I got these scars? Well... it was from doing the Hot Ones challenge at ZBT. Join us at the house for dinner, where we'll be packing the heat (and chicken)!
 
 Title: Bowling
-Time: Monday, September 6, 8:00pm - 11:00pm
-Location: Meet at Walker
+Time: Monday, September 5, 7:00pm - 10:00pm
+Location: Meet at ZBT (from dinner) or Kresge Turnaround
 Description:
-Have some spare time? Come bowl with the brothers of ZBT! Whether you strike it lucky or end up in the gutter, it’ll be a great time! Need a ride? Call us at 617-420-2928!
+Have some spare time? Come bowl with the brothers of ZBT! Whether you strike it lucky or end up in the gutter, it’ll be a great time! 
 
 Title: Late Night IHOP
-Time: Monday, September 6, 11:00pm - 12:00am
-Location: Meet at Walker
+Time: Monday, September 5, 10:00pm - 12:00am
+Location: Meet at Kresge Turnaround
 Description:
-Up late? Hungry from bowling? Join ZBT in eating America’s best pancakes at a late-night run to IHOP. Bring your appetites because this meal is on us! We will be providing transportation from campus to IHOP and from bowling directly to IHOP as well. Call 617-420-2928 for a ride.
+Up late? Hungry from bowling? Join ZBT in eating America’s best pancakes at a late-night run to IHOP. Bring your appetites because this meal is on us! We will be providing transportation from campus to IHOP and from bowling directly to IHOP as well. 
 
-Title: *ZBT Breakfast of Champions
-Time: Tuesday, September 7, 9:00am - 11:00am
-Location: Student Center Steps
+Title: Rock Climbing
+Time: Tuesday, September 6, 10:00am - 12:00pm
+Location: Meet at Kresge Turnaround
 Description:
-Come to the stud for ZBT’s Breakfast of Champions! Eggs, bacon, sausage, fresh fruit and muffins for days. Vegetarians welcome! Call us at 617-420-2928
+What better thing to do at 10 in the morning than climb rocks? 
 
-Title: *Skyzone
-Time: Tuesday, September 7, 5:30pm - 8:00pm
-Location: Meet at Walker
+Title: Skyzone
+Time: Tuesday, September 6, 2:00pm - 5:00pm
+Location: Meet at Kresge Turnaround
 Description:
-Massive wall-to-wall trampolines, cushy foam pits, the ultimate in dodgeball. And of course, a basketball hoop to dunk. Come to Skyzone with ZBT! Call 617-420-2928 for your ride.
-
-Title: *Dinner with ZBT
-Time: Tuesday, September 7, 8:00pm - 10:00pm
-Location: Meet at Walker
-Description:
-Call ZBT for rides at 617-420-2928.
-
-Title: *Dinner with ZBT
-Time: Wednesday, September 8, 5:30pm - 7:30pm
-Location: Meet at Walker
-Description:
-Call ZBT for rides at 617-420-2928.
-
-Title: *Escape Room
-Time: Wednesday, September 8, 7:30pm - 10:00pm
-Location: Meet at Walker
-Description:
-Using only your wits and your friends, can you break free in under an hour? We’ll meet on campus and then head out to Escape the Room Boston. Those who make it out will be driven back to MIT; those left inside may never be seen again. What will your fate be? Call ZBT for rides at 617-420-2928.
-
-Title: *Dinner with ZBT
-Time: Thursday, September 9, 5:30pm - 7:30pm
-Location: Meet at Walker
-Description:
-Call ZBT for rides at 617-420-2928.
-
-Title: *Minigolf
-Time: Thursday, September 9, 7:30pm - 10:00pm
-Location: Meet at Walker
-Description:
-Ever wanted to putt like Tiger? Come play minigolf with the brothers of ZBT. For rides, call us at 617-420-2928!
-`
+Massive wall-to-wall trampolines, cushy foam pits, the ultimate in-door dodgeball. And of course, a basketball hoop to dunk. Come to Skyzone with ZBT!
+`;
